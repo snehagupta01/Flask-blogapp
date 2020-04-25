0c787de32ebc7af580e15736e842575c57bf54c7
snehagupta01 <sneha2001gupta@gmail.com>
2020-04-25 12:07:53 +0530

    added template for home page

--- flaskmain.py
+++ flaskmain.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
@@ -8,7 +8,7 @@ def index():
 
 @app.route("/home")
 def home():
-    return "<h1>Home page</h1>"
+    return render_template("home.html")
 
 @app.route("/user")
 def user():
