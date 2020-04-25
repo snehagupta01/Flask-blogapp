a40cce509f593993f3f93032bf8795fa30dc379d
snehagupta01 <sneha2001gupta@gmail.com>
2020-04-25 11:44:32 +0530

    adding more routes

--- flaskmain.py
+++ flaskmain.py
@@ -6,5 +6,13 @@ app=Flask(__name__)
 def index():
     return "<h1>hello world</h1>"
 
+@app.route("/home")
+def home():
+    return "<h1>Home page</h1>"
+
+@app.route("/user")
+def user():
+    return "<h1>Profile page</h1>"
+
 if __name__=='__main__':
     app.run(debug=True)
