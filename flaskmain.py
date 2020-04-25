8f1f121d2b065c478e2889a855d0c72140b26a60
snehagupta01 <sneha2001gupta@gmail.com>
2020-04-25 12:12:58 +0530

    added template for error pages

--- flaskmain.py
+++ flaskmain.py
@@ -16,11 +16,11 @@ def about():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return "<h2>Page not found</h2>",404
+    return render_template("404.html"),404
 
 @app.errorhandler(500) 
 def internal_server_error(e):    
-    return "<h1>Internal server error</h1>", 500
+    return render_template("500.html"),500
 
 if __name__=='__main__':
     app.run(debug=True)
