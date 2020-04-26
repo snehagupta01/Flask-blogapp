e658694a4866057599b074ea1dc49bb2a0c14e04
snehagupta01 <sneha2001gupta@gmail.com>
2020-04-26 15:10:37 +0530

    updated routes.py

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect,request
 from flask_blog.models import User,Post
 from flask_blog.forms import RegistrationForm, LoginForm
 from flask_blog import app,db,bcrypt
@@ -55,7 +55,8 @@ def login():
         user=User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password,form.password.data):
             login_user(user,remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_pg=request.args.get('next')
+            return redirect(next_pg) if next_pg else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -66,5 +67,6 @@ def logout():
     return redirect(url_for('home'))
 
 @app.route("/account")
+@login_required
 def account():
     return render_template("account.html",title="Account")
