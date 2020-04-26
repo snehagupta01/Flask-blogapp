349e4a5611d37b76aec69aaf30365114e68c8cdb
snehagupta01 <sneha2001gupta@gmail.com>
2020-04-26 14:18:57 +0530

    +proper login

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -2,11 +2,13 @@ from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager=LoginManager(app)
 
 from flask_blog import routes
--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flask_blog.models import User,Post
 from flask_blog.forms import RegistrationForm, LoginForm
 from flask_blog import app,db,bcrypt
+from flask_login import login_user
 
 posts = [
     {
@@ -47,9 +48,10 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
+        user=User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password,form.password.data):
+            login_user(user,remember=form.remember.data)
             return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
