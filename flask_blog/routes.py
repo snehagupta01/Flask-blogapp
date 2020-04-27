99ec51e5c1fd9eb088d72205eb2b01b10e5e9104
snehagupta01 <sneha2001gupta@gmail.com>
2020-04-27 11:49:54 +0530

    +reset_request.html

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -2,7 +2,7 @@ import os
 import secrets
 from flask import render_template, url_for, flash, redirect,request,abort
 from flask_blog.models import User,Post
-from flask_blog.forms import RegistrationForm, LoginForm ,UpdateAccountForm,PostForm
+from flask_blog.forms import RegistrationForm, LoginForm ,UpdateAccountForm,PostForm,RequestResetForm,ResetPasswordForm
 from flask_blog import app,db,bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
 from PIL import Image
@@ -146,3 +146,10 @@ def user_posts(username):
         .order_by(Post.date_posted.desc())\
         .paginate(page=page, per_page=3)
     return render_template('user_posts.html', posts=posts, user=user)
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    return render_template('reset_request.html', title='Reset Password', form=form)
